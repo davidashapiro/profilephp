353b1fb9f422bb828dcd46f7082d61534c3856d9
David Shapiro <davidashapiro@users.noreply.github.com>
2018-11-19 03:15:13 -0600

    commit

---
+++ footer.php
@@ -0,0 +1,74 @@
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</table>
+												</td>
+												<td valign='top'>
+													<table cellpadding='0' cellspacing='0' border='0' width='2' height='100%'>
+														<tr>
+															<td>
+																<div style='height:2px; width:2px;'>
+																	<spacer></spacer>
+																</div>
+															</td>
+														</tr>
+														<tr height='100%' bgcolor='#666666'>
+															<td></td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div style='height:2px; width:2px; '>
+														<spacer></spacer>
+													</div>
+												</td>
+												<td bgcolor='#666666' width='100%'></td>
+												<td bgcolor='#666666'></td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+
+	<div style='align: center; '> 
+		<ul id='footerLinks'> 
+			<li><a href='index.php'>About Me</a></li> 
+			<li><a href='csoon.php'>Blog</a></li> 
+			<li><a href='csoon.php'>FAQ</a></li> 
+			<li><a href='csoon.php'>Terms</a></li> 
+			<li><a href='csoon.php'>Privacy Policy</a></li> 
+		</ul> 
+	</div> 
+	<div class='footer-bottom'> 
+		<div class='row'> 
+			<div class='col-twelve'> 
+				<div class='copyright'> 
+					<span>&copy; Copyright 2018</span> 
+					<span>Design by David Shapiro</span> 
+				</div> 
+				<div id='go-top'> 
+					<a class='smoothscroll' title='Back to Top' href='#top'><i class='icon-arrow-up'></i></a> 
+				</div> 
+			</div> 
+		</div> 
+	</div> <!-- end footer-bottom -->
+	<script src='' type='text/javascript'>
+		var data = '&r=' + escape(document.referrer) + '&n=' + escape(navigator.userAgent) + '&p=' + escape(navigator.userAgent) + '&g=' + escape(document.location.href);
+
+		if (navigator.userAgent.substring(0,1)>'3')
+			data = data + '&sd=' + screen.colorDepth  + '&sw=' + escape(screen.width+'x'+screen.height);
+
+		document.write('<a href='http://www.1freecounter.com/stats.php?i=138315' target=\'_blank\' >');
+		document.write('<img alt='Free Counter' border=0 hspace=0 '+'vspace=0 src='http://www.1freecounter.com/counter.php?i=138315' + data + ''>');
+		document.write('</a>');
+	</script>
